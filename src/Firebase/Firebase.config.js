d6b7b0d0f429996132f94b0bfbf98ca16e805a1d
ahmedparbes <fbhelpsbd@gmail.com>
2021-11-13 22:18:29 +0600

    add

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCIKNyzEwMi-8ppzl6PRCKczarvsTGLWbw",
-    authDomain: "jerins-parlour-aa7be.firebaseapp.com",
-    projectId: "jerins-parlour-aa7be",
-    storageBucket: "jerins-parlour-aa7be.appspot.com",
-    messagingSenderId: "2629682362",
-    appId: "1:2629682362:web:5c7d9b2e48197111546e83"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
